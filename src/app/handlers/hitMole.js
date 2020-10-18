890e163d4b6d09280c1de3e38cc12bd3fb0154b8
matgor <mat.gor96@gmail.com>
2020-10-18 13:43:52 +0200

    Fixed touch event bug adding points after game ends

--- src/app/handlers/hitMole.js
+++ src/app/handlers/hitMole.js
@@ -2,6 +2,7 @@ import handleScore from './handleScore';
 
 const hitMole = (event) => {
     event.target.removeEventListener('click', hitMole);
+    event.target.removeEventListener('touchstart', hitMole);
     event.target.style.backgroundImage = 'none';
 
     const gameBoard = event.target.parentElement;
--- src/app/handlers/placeMole.js
+++ src/app/handlers/placeMole.js
@@ -6,16 +6,16 @@ export default (moleImage, gameSpeed) => {
 	return setInterval(() => {
 		const gameFields = document.querySelectorAll('.game-field');
 		const prevRand = rand;
-        rand = checkIfDifferent(prevRand);
+    rand = checkIfDifferent(prevRand);
 
 		gameFields.forEach(field => {
-            field.style.backgroundImage = 'none';
+      field.style.backgroundImage = 'none';
 			field.removeEventListener('click', hitMole);
 			field.removeEventListener('touchstart', hitMole);
 		});
     
-        const molePosition = gameFields[rand];
-        molePosition.style.backgroundImage = `url(${moleImage})`;
+    const molePosition = gameFields[rand];
+    molePosition.style.backgroundImage = `url(${moleImage})`;
 		molePosition.addEventListener('click', hitMole);
 		molePosition.addEventListener('touchstart', hitMole);
 	}, gameSpeed);
